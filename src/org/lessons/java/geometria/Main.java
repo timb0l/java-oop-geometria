2b96593e41eb703b56438bbb9027703a3bdc8da4
Jam Timbol <jam.timbol00@gmail.com>
2023-07-18 14:15:20 +0200

    Done

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,8 +1,30 @@
 package org.lessons.java.geometria;
+
+import java.util.Scanner;
+
 /*
 * ask W/H
 * Add costruttore w/ Area n Parameter
 * do the square*/
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//ask deets
+        Square square = new Square();
 
+        System.out.println("Insert height");
+            int heigthSquare = Integer.parseInt(scan.nextLine());
+        System.out.println("Insert width");
+            int widthSquare = Integer.parseInt(scan.nextLine());
+
+        square.height = heigthSquare;
+        square.width = widthSquare;
+
+//print area and perimiter
+        System.out.println("Area: " + square.areaSquare());
+        System.out.println("Perimiter: " + square.perimiterSquare());
+
+        scan.close();
+
+    }
 }
---
+++ src/org/lessons/java/geometria/Square.java
@@ -0,0 +1,17 @@
+package org.lessons.java.geometria;
+/*
+* do square
+* */
+public class Square {
+//mesurments
+    int height;
+    int width;
+//area
+    int areaSquare(){
+        return width * height;
+    }
+//parameter
+    int perimiterSquare(){
+        return (width + height) * 2;
+    }
+}
